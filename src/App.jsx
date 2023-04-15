f7e3297f0d8ea211a3d2e2b4d59cbd91f257e637
vusal5555 <vusal.novruzov.1@iliauni.edu.ge>
2023-04-15 16:38:03 +0400

    added authentication

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,24 @@
-import CategoryDirectory from "./components/category-directory/category-directory.component";
+import Home from "./routes/home/home.component";
+import { Routes, Route } from "react-router-dom";
+import Navigation from "./routes/navigation/navigation.component";
+import SignIn from "./routes/sign-in/sign-in.component";
+
+const Shop = () => {
+  return <h1>I am the Shop Page!</h1>;
+};
+
+<Navigation></Navigation>;
 
 const App = () => {
-  const categories = [
-    {
-      id: 1,
-      title: "Hats",
-      imageUrl: "https://i.ibb.co/cvpntL1/hats.png",
-    },
-    {
-      id: 2,
-      title: "Jackets",
-      imageUrl: "https://i.ibb.co/px2tCc3/jackets.png",
-    },
-    {
-      id: 3,
-      title: "Sneakers",
-      imageUrl: "https://i.ibb.co/0jqHpnp/sneakers.png",
-    },
-    {
-      id: 4,
-      title: "Womens",
-      imageUrl: "https://i.ibb.co/GCCdy8t/womens.png",
-    },
-    {
-      id: 5,
-      title: "Mens",
-      imageUrl: "https://i.ibb.co/R70vBrQ/men.png",
-    },
-  ];
-
-  return <CategoryDirectory categories={categories}></CategoryDirectory>;
+  return (
+    <Routes>
+      <Route path="/" element={<Navigation></Navigation>}>
+        <Route index element={<Home></Home>}></Route>
+        <Route path="shop" element={<Shop></Shop>}></Route>
+        <Route path="signIn" element={<SignIn></SignIn>}></Route>
+      </Route>
+    </Routes>
+  );
 };
 
 export default App;
---
+++ src/routes/navigation/navigation.component.jsx
@@ -0,0 +1,27 @@
+import { Outlet, Link } from "react-router-dom";
+import { Fragment } from "react";
+import { ReactComponent as ReactLogo } from "../../assets/crown.svg";
+import "./navigation.styles.scss";
+
+const Navigation = () => {
+  return (
+    <Fragment>
+      <div className="navigation">
+        <Link className="logo-container" to="/">
+          <ReactLogo className="logo"></ReactLogo>
+        </Link>
+        <div className="nav-links-container">
+          <Link className="nav-link" to="/shop">
+            SHOP
+          </Link>
+          <Link className="nav-link" to="/signIn">
+            SIGN IN
+          </Link>
+        </div>
+      </div>
+      <Outlet></Outlet>
+    </Fragment>
+  );
+};
+
+export default Navigation;
